972ab43c2637562b336c7077ed3412d30e2dcc98
Luka Belani <luka.belani@gmail.com>
2020-02-21 21:13:41 +0100

    add react-spring animation and update css

--- src/components/ShowGear.js
+++ src/components/ShowGear.js
@@ -1,13 +1,33 @@
 import React from "react";
+import { useSpring, animated } from "react-spring";
 import { gear } from "./gear";
 
+const calc = (x, y) => [
+  -(y - window.innerHeight / 2) / 20,
+  (x - window.innerWidth / 2) / 20,
+  1.1
+];
+const trans = (x, y, s) =>
+  `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`;
+
 export default function ShowGear() {
+  const [props, set] = useSpring(() => ({
+    xys: [0, 0, 1],
+    config: { mass: 5, tension: 350, friction: 40 }
+  }));
+
   let gearList = gear.items.map(item => {
     return (
-      <div className="card" key={item.id}>
+      <animated.div
+        className="card"
+        key={item.id}
+        onMouseMove={({ clientX: x, clientY: y }) => set({ xys: calc(x, y) })}
+        onMouseLeave={() => set({ xys: [0, 0, 1] })}
+        style={{ transform: props.xys.interpolate(trans) }}
+      >
         <img className="card-image" src={item.img} alt={item.title} />
         <span className="card-title">{item.title}</span>
-      </div>
+      </animated.div>
     );
   });
 
